188bd6f5381b2f56f697780442704fbc04687001
Lukáš Jarmara <jarmaralukas123@gmail.com>
2024-05-15 16:16:37 +0200

    Added routing and initialized first page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import * as React from "react";
+import Choose from "./Components/Choose/Choose";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Choose />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
