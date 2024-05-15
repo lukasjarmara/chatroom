a0bed035f032b41fc6a09118ba11e21eb71176ba
Lukáš Jarmara <jarmaralukas123@gmail.com>
2024-05-15 17:09:42 +0200

    Updated index.css to use tailwind Made Choose work completely, now only needs styling Updated App to be able to get roomId from Choose

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import * as React from "react";
 import Choose from "./Components/Choose/Choose.tsx";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
 import Room from "./Components/Room/Room.tsx";
+
 function App() {
   const [room, setRoom] = React.useState(0);
+  const handleCallback = (childprop) => {
+    setRoom(childprop);
+  };
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Choose />} />
+        <Route path="/" element={<Choose handleCallback={handleCallback} />} />
         <Route path={`/room-${room}`} element={<Room roomId={room} />} />
       </Routes>
     </BrowserRouter>
--- src/Components/Choose/Choose.tsx
+++ src/Components/Choose/Choose.tsx
@@ -1,9 +1,29 @@
 import * as React from "react";
 
-const Choose: React.FC = () => {
+interface ChooseProps {
+  handleCallback: (room: number) => void;
+}
+const Choose: React.FC<ChooseProps> = ({ handleCallback }) => {
+  const [room, setRoom] = React.useState(0);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    handleCallback(room);
+  };
   return (
-    <div>
-      <p>Texts</p>
+    <div className="w-screen h-screen flex justify-center items-center flex-col">
+      <h1 className="content-center">Select your room</h1>
+      <form onSubmit={handleSubmit}>
+        <div className="flex flex-row">
+          <button type="button" onClick={(e) => setRoom(room + 1)}>
+            +
+          </button>
+          <p>{room}</p>
+          <button type="button" onClick={(e) => setRoom(room - 1)}>
+            -
+          </button>
+        </div>
+        <button type="submit">Chat</button>
+      </form>
     </div>
   );
 };
