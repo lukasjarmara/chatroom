584483d057fff9b9870e4b9e5c28d5271a4ae43f
Lukáš Jarmara <jarmaralukas123@gmail.com>
2024-05-16 16:05:32 +0200

    Firebase setup

---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB0JDUBYuYP0gq4JucEOvf02REM2V1PY5Q",
+  authDomain: "chatroom-596d1.firebaseapp.com",
+  projectId: "chatroom-596d1",
+  storageBucket: "chatroom-596d1.appspot.com",
+  messagingSenderId: "103024812152",
+  appId: "1:103024812152:web:4bf474806507c312244d8d",
+  measurementId: "G-8TWRYVGL4L",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
