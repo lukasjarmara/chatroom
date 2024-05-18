7b7227a4d00d0818e95efdd5871906c924f94d59
Lukáš Jarmara <jarmaralukas123@gmail.com>
2024-05-18 13:28:28 +0200

    Setup realtime database and reading from it

--- src/Components/Room/Room.tsx
+++ src/Components/Room/Room.tsx
@@ -2,20 +2,23 @@ import * as React from "react";
 import { db } from "../../firebase";
 import { ref, set, onValue } from "firebase/database";
 import { useParams, useLocation } from "react-router-dom";
-
+interface Message {
+  message: string;
+  user: string;
+}
 const Room: React.FC = () => {
   const { roomId } = useParams();
   const query = new URLSearchParams(useLocation().search);
   const user = query.get("user");
   const [response, setResponse] = React.useState("");
-  const [messages, setMessages] = React.useState([]);
+  const [messages, setMessages] = React.useState<Message[]>([]);
 
   React.useEffect(() => {
-    const query = ref(db, `/${roomId}`);
+    const query = ref(db, `/room-${roomId}`);
     return onValue(query, (snapshot) => {
       const data = snapshot.val();
-      setMessages(data);
-      console.log(data);
+      const messagesArray: Message[] = Object.values(data || {});
+      setMessages(messagesArray);
     });
   }, [roomId]);
 
@@ -23,6 +26,13 @@ const Room: React.FC = () => {
     <div className="w-screen h-screen ">
       <h1>{roomId}</h1>
       <div className="flex justify-center items-center flex-col">
+        {messages.map((message, index) => (
+          <div key={index}>
+            <p>
+              {message.user}: {message.message}
+            </p>
+          </div>
+        ))}
         <input
           className="bg-black text-white"
           onChange={(e) => setResponse(e.target.value)}
