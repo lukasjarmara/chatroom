b3c895a3767430d4cde5ed9ee2eb1bca721baade
Lukáš Jarmara <jarmaralukas123@gmail.com>
2024-05-19 12:51:54 +0200

    Added a loading screen and sending messages to database

--- src/Components/Room/Room.tsx
+++ src/Components/Room/Room.tsx
@@ -12,33 +12,58 @@ const Room: React.FC = () => {
   const user = query.get("user");
   const [response, setResponse] = React.useState("");
   const [messages, setMessages] = React.useState<Message[]>([]);
+  const [isLoading, setIsLoading] = React.useState(true);
+  const location = `/room-${roomId}`;
+  const [lastMessage, setLastMessage] = React.useState(0);
 
   React.useEffect(() => {
-    const query = ref(db, `/room-${roomId}`);
-    return onValue(query, (snapshot) => {
+    return onValue(ref(db, location), (snapshot) => {
       const data = snapshot.val();
       const messagesArray: Message[] = Object.values(data || {});
       setMessages(messagesArray);
+      setIsLoading(false);
     });
-  }, [roomId]);
+  }, [roomId, location]);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    set(ref(db, `/room-${roomId}/message-${lastMessage + 1}`), {
+      messages: response,
+      user: user,
+    });
+    setResponse("");
+  };
 
   return (
-    <div className="w-screen h-screen ">
-      <h1>{roomId}</h1>
-      <div className="flex justify-center items-center flex-col">
-        {messages.map((message, index) => (
-          <div key={index}>
-            <p>
-              {message.user}: {message.message}
-            </p>
+    <div className="w-screen h-screen">
+      {isLoading ? (
+        <div className="flex justify-center items-center">
+          <div className="w-12 h-12 border-t-4 border-blue-500 rounded-full animate-spin"></div>{" "}
+        </div>
+      ) : (
+        <>
+          <div className="flex flex-col justify-between items-center">
+            <h1>{roomId}</h1>
+            <div>
+              {messages.map((message, index) => (
+                <div key={index}>
+                  <h4>{message.user}</h4>
+                  <p> {message.message}</p>
+                </div>
+              ))}
+            </div>
+            <form onSubmit={handleSubmit} className="fixed bottom-5">
+              <input
+                className="bg-black text-white"
+                onChange={(e) => setResponse(e.target.value)}
+                value={response}
+              />
+              <button>
+                <span className="material-symbols-outlined">send</span>
+              </button>
+            </form>
           </div>
-        ))}
-        <input
-          className="bg-black text-white"
-          onChange={(e) => setResponse(e.target.value)}
-          value={response}
-        />
-      </div>
+        </>
+      )}
     </div>
   );
 };
