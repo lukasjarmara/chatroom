296ff2e120c8f1d6f51307c41d9bdf2a28253555
Lukáš Jarmara <45052496+lukasjarmara@users.noreply.github.com>
2024-05-21 19:27:48 +0200

    Updated firebase.js so that anyone can put their own config
    Removed my credentials, made a new api key different then those in my previous commits

--- src/firebase.js
+++ src/firebase.js
@@ -8,19 +8,12 @@ import { getDatabase } from "firebase/database";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyB0JDUBYuYP0gq4JucEOvf02REM2V1PY5Q",
-  authDomain: "chatroom-596d1.firebaseapp.com",
-  projectId: "chatroom-596d1",
-  storageBucket: "chatroom-596d1.appspot.com",
-  messagingSenderId: "103024812152",
-  appId: "1:103024812152:web:4bf474806507c312244d8d",
-  measurementId: "G-8TWRYVGL4L",
+// Your Config
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 export const db = getDatabase(
-  app,
-  "https://chatroom-596d1-default-rtdb.europe-west1.firebasedatabase.app"
+  app
 );
